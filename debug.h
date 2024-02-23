d4e0bcd4b7814a25cd627b5cf42cf7306bbe20dc
jchong-rock <141347545+jchong-rock@users.noreply.github.com>
2024-02-23 17:03:56 +0000

    jchong-rock: print to stderr instead of stdout

--- debug.h
+++ debug.h
@@ -2,6 +2,6 @@
 #define JCHONG_DEBUG_CPP_H
 
 #include <iostream>
-#define DEBUG(X) (std::cout << __FILE__ << "-" << __LINE__ << ": " #X << " = " << X << '\n')
+#define DEBUG(X) (std::cerr << __FILE__ << "-" << __LINE__ << ": " #X << " = " << X << '\n')
 
 #endif // JCHONG_DEBUG_CPP_H
