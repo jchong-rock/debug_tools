279c7067b609a92ec783b5c8408dc5ff130ed044
Jake <jpc21@ic.ac.uk>
2024-02-23 16:56:50 +0000

    jchong-rock: modify debug for Objective-C

--- debug.h
+++ debug.h
@@ -1,7 +1,13 @@
-#ifndef JCHONG_DEBUG_CPP_H
-#define JCHONG_DEBUG_CPP_H
+#ifndef JCHONG_DEBUG_OBJC_H
+#define JCHONG_DEBUG_OBJC_H
 
-#include <iostream>
-#define DEBUG(X) (std::cout << __FILE__ << "-" << __LINE__ << ": " #X << " = " << X << '\n')
+#define DEBUG(X) _Generic((X),                                                      \
+                    char*: NSLog(@"%s-%s: %s = %s\n", __FILE__, __LINE__, #X, X)    \
+                    long: NSLog(@"%s-%s: %s = %ld\n", __FILE__, __LINE__, #X, X)    \
+                    int: NSLog(@"%s-%s: %s = %d\n", __FILE__, __LINE__, #X, X),     \
+                    double: NSLog(@"%s-%s: %s = %f\n", __FILE__, __LINE__, #X, X),  \
+                    char: NSLog(@"%s-%s: %s = %c\n", __FILE__, __LINE__, #X, X),    \
+                    default: NSLog(@"%s-%s: %s = %@\n", __FILE__, __LINE__, #X, X)  \
+                )(X) 
 
-#endif // JCHONG_DEBUG_CPP_H
+#endif // JCHONG_DEBUG_OBJC_H
