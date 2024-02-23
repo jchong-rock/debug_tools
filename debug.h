1e0b9b2b29deaecf593b777fa88d791a583d8091
Jake <jpc21@ic.ac.uk>
2024-02-23 16:42:56 +0000

    jchong-rock: debug statement to show value of variable at given position

---
+++ debug.h
@@ -0,0 +1,7 @@
+#ifndef JCHONG_DEBUG_CPP_H
+#define JCHONG_DEBUG_CPP_H
+
+#include <iostream>
+#define DEBUG(X) (std::cout << __FILE__ << "-" << __LINE__ << ": " #X << " = " << X << '\n')
+
+#endif // JCHONG_DEBUG_CPP_H
